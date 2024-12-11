1ee45d354218e18a4f809959d14523148f8a80ca
Fernando Veloso <fernandovelosomkt@gmail.com>
2024-12-11 00:42:49 -0300

    Primeiro commit

---
+++ deliverydoveloso.py
@@ -0,0 +1,10 @@
+print('𝑨𝒑𝒑 𝑫𝒆𝒍𝒊𝒗𝒆𝒓𝒚 𝒅𝒐 𝑽𝒆𝒍𝒐𝒔𝒐\n')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {opcao_escolhida}')
+

900086eae64d97f9dec5760f7261a127ee611498
Fernando Veloso <fernando.veloso@vidibr.com>
2024-12-13 02:46:43 -0300

    Inclusão de novas linhas de código. Expansão do projeto

--- deliverydoveloso.py
+++ deliverydoveloso.py
@@ -1,18 +1,34 @@
-print('𝑨𝒑𝒑 𝑫𝒆𝒍𝒊𝒗𝒆𝒓𝒚 𝒅𝒐 𝑽𝒆𝒍𝒐𝒔𝒐\n')
+import os
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+def exibir_nome_do_programa():
+    print('𝑨𝒑𝒑 𝑫𝒆𝒍𝒊𝒗𝒆𝒓𝒚 𝒅𝒐 𝑽𝒆𝒍𝒐𝒔𝒐\n')
 
-opcao_escolhida = int(input('Escolha uma opção: '))
-print(f'Você escolheu a opção {opcao_escolhida}')
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurantes')
-elif opcao_escolhida == 2:
-    print('Listar restaurantes')
-elif opcao_escolhida == 3:
-    print('Ativar restaurante')
-else:
-    print('Encerrando o programa')
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando o App\n')
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurantes')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        finalizar_app()
+        print('Encerrando o programa')
+
+def main()
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__'
+    main()
